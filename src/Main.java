ba38f880850c90320340194ed75218dfd4938e38
Andrej Beliančin <3210ado@gmail.com>
2018-10-03 18:52:18 +0200

    Pridanie kniznice jfoenix a start aplikacie ako javaFx

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
-public class Main {
+import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTreeView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
 
-    public static void main(String args[]) {
-        System.out.println("Bezim");
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        JFXTreeView k = new JFXTreeView();
+        JFXTabPane tabs = new JFXTabPane();
+        tabs.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+        Tab tab = new Tab();
+        tab.setText(" Nadpis");
+        tabs.getTabs().add(tab);
+
+
+        Scene scene = new Scene(tabs, 600, 800); // Manage scene size
+        primaryStage.setTitle("Algoritmy a údajové štruktúry 2- Semestrálna práca 1");
+
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
     }
 
 }
